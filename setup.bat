a17416c10d0e071407b33733d9d34f6566c6e17d
user <101312928+user001js@users.noreply.github.com>
2022-05-15 00:48:27 -0300

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,7 @@
+pip install discord
+pip install asyncio
+pip install requests
+pip install colorama
+pip install pyfiglet
+pip install selenium
+pip install datetime
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+title FBI Account Nuker by user 001 and duda 002
+rem FBI Account Nuker made by user 001 with the co-creation of duda 002
+python fbi-account-nuker.pyc
